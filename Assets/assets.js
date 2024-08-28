bbefe2a40facda2dae1c1eaf32908d2a3d00d536
joanvasquez21 <joanvrivas.21@gmail.com>
2024-08-28 17:23:16 +0200

    feat: header section is created

---
+++ Assets/assets.js
@@ -0,0 +1,33 @@
+import blog_pic_1 from './springboot_api_rest.png';
+import facebook_icon from './facebook_icon.png'
+import googleplus_icon from './googleplus_icon.png'
+import github from './github_.png'
+import profile_icon from './profile_icon.png'
+import arrow from './arrow.png'
+import logojov from './image.png'
+import logojov_white from './logo_jov_white.png'
+
+export const assets = {
+    blog_pic_1,
+    facebook_icon,
+    googleplus_icon,
+    github,
+    profile_icon,
+    arrow,
+    logojov,
+    logojov_white,
+    
+    }
+
+    export const blog_data = [{
+        id:1,
+        title:"Prueba",
+        description:"Prueba",
+        image:blog_pic_1,
+        date:Date.now(),
+        category:"Java",
+        author:"jov",
+        author_img:profile_icon
+    },
+ 
+]
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,3 +1,4 @@
+import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'    
 
@@ -5,7 +6,7 @@ const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>Header 
         <div className="flex justify-between items-center">
-            <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+            <Image src={assets.logojov} width={180} alt='' className='w-[130px] sm:w-auto' />
         </div>
     </div>
   )
