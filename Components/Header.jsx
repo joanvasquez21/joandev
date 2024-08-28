1432fa4bae08718535b83c335e14b9ae194e52ee
joanvasquez21 <joanvrivas.21@gmail.com>
2024-08-28 17:17:42 +0200

    feat: header section is created

---
+++ Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'    
+
+const Header = () => {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>Header 
+        <div className="flex justify-between items-center">
+            <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+        </div>
+    </div>
+  )
+}
+
+export default Header
